8151e56657b28e1fb4047ae7bfa8a02260488f7e
Henrique Laroque <henriquelaroque@outlook.com>
2025-09-10 15:50:49 -0300

    Style: Header and NewBoard new style

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,6 +1,7 @@
 // import { useForm } from "react-hook-form";
 // import { toast } from "sonner";
 import { IoSearchSharp } from "react-icons/io5";
+import { FaRegCalendarCheck } from "react-icons/fa";
 import logo from "../img/logoMyT.png"
 
 import { Link, useNavigate } from "react-router-dom";
@@ -26,19 +27,22 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-[#F5F7FA] dark:bg-gray-700">
+    <header className="bg-[#F5F7FA] dark:bg-gray-700 py-4">
       <div className="w-[80%] mx-auto px-6 flex items-center justify-between">
 
-        <div className="flex items-center">
-          <img src={logo} className="w-25 h-20 rounded-full" />
+        <div className="flex items-center gap-2 ">
+          {/* <img src={logo} className="w-25 h-20 rounded-full" /> */}
+          <FaRegCalendarCheck size={24} className="text-[#3B82F6]" />
+          <h1 className="text-[#3B82F6] text-[1.5rem] font-bold">MyT</h1>
         </div>
 
         <div className="flex flex-1 justify-center">
           <div className="flex items-center gap-4">
+
             <button
               type="button"
               className="text-[#3B82F6] font-bold text-[1rem] cursor-pointer px-4 py-2 rounded-lg 
-            transition-colors duration-300 hover:bg-gray-300 hover:text-white"
+            transition-colors duration-300 hover:bg-[#155fd6] hover:text-white hover:shadow-md"
             >
               Criar
             </button>
@@ -49,14 +53,14 @@ export default function Header() {
                 type="search"
                 placeholder="Pesquisa"
                 className="h-10 w-[30rem] rounded-[5px] border-2 border-[#3B82F6] pl-8 pr-10 text-sm text-[#3B82F6] placeholder-[#3B82F6]
-              outline-none focus:ring-gray-500 focus:ring-2 focus:border-none transition-colors duration-300 hover:border-gray-500"
+              outline-none focus:ring-[#3B82F6] focus:ring-2 focus:border-none transition-all duration-300 hover:border-[#155fd6] hover:shadow-md"
               />
             </div>
 
             <Link
               to="/boards"
               className="text-[#3B82F6] font-bold text-[1rem] cursor-pointer px-4 py-2 rounded-lg
-            transition-colors duration-300 hover:bg-gray-300 hover:text-white"
+            transition-colors duration-300 hover:bg-[#155fd6] hover:text-white hover:shadow-md"
             >
               Boards
             </Link>
@@ -67,13 +71,13 @@ export default function Header() {
           {usuario.id ? (
             <>
               <span className="text-[#3B82F6] font-bold text-[1rem] cursor-pointer px-4 py-2 rounded-lg
-            transition-colors duration-300 hover:bg-gray-300 hover:text-white mr-4">
+            transition-colors duration-300 hover:bg-[#155fd6] hover:text-white mr-4">
                 Olá {primeiroNome(usuario.nome)}
               </span>
               <button
                 onClick={usuarioSair}
                 className="text-[#3B82F6] font-bold text-[1rem] cursor-pointer px-4 py-2 rounded-lg
-              transition-colors duration-300 hover:bg-gray-300 hover:text-white"
+              transition-colors duration-300 hover:bg-[#155fd6] hover:text-white"
               >
                 Sair
               </button>
@@ -82,7 +86,7 @@ export default function Header() {
             <Link
               to="/login"
               className="text-[#3B82F6] font-bold text-[1rem] cursor-pointer px-4 py-2 rounded-lg
-            transition-colors duration-300 hover:bg-gray-300 hover:text-white"
+            transition-colors duration-300 hover:bg-[#155fd6] hover:text-white hover:shadow-md"
             >
               Login
             </Link>
